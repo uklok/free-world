1648307dcffbc00c5d99c4e9940a7b0b8e89cde8
👻 <y@uklok.ai>
2023-12-11 01:25:14 -0400

    feat(architecture): 👻 Images & Video
    - The images and video of the architecture where included in the project.
    - The README.md was updated with the dev-post.
    - The solidity compiler version was updated.
    
    This ref: #4

--- packages/hardhat/contracts/FreeWorld.sol
+++ packages/hardhat/contracts/FreeWorld.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import "./interfaces/IFreeWorld.sol";
 import {IFreeWorldUserRegistry} from "./interfaces/IFreeWorldUserRegistry.sol";
